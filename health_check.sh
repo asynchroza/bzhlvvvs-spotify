693141b2e70f2696e8928596b5f061479a41c726
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-06-20 18:00:01 +0300

    Add health check

---
+++ health_check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+URL="${LAMBDA_URL}"
+response=$(curl -s -w "%{http_code}" $URL)
+
+status_code=${response: -3}  
+
+if [ $status_code -eq 200 ]; then
+    echo "Request successful (Status Code: 200)"
+else
+    echo "Request failed with the following status code: ${status_code}"
+    echo "Response: ${response%???}"
+    exit 1
+fi
